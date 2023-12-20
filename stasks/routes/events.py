9c4b2e400c30bc5a0437d9dbe13bbdfc29caf07b
Rick Dawson <dawsora@gmail.com>
2023-12-20 11:14:42 -0500

    made events deletable

--- stasks/routes/events.py
+++ stasks/routes/events.py
@@ -7,13 +7,17 @@ events = Blueprint('events', __name__)
 @events.route('/events/<int:id>')
 def event_detail(id):
     event = Event.query.get(id)
-    return render_template('event_detail.html', event=event)
+    return render_template('detail_event.html', event=event)
 
-@events.route("/events", methods=["GET", "POST"])
+@events.route('/events')
+def event_list():
+    events = Event.query.all()
+    return render_template('events.html', events=events)
+
+@events.route("/events/add", methods=["GET", "POST"])
 def add_event():
     if request.method == "GET":
-        events = Event.query.all()
-        return render_template("add_event.html", events=events, people=Person.get_names())
+        return render_template("add_event.html",  people=Person.get_names())
     elif request.method == "POST":
         name = request.form.get("name")
         description = request.form.get("description")
@@ -26,3 +30,38 @@ def add_event():
         db.session.commit()
         message = "Event added successfully"
     return render_template("add_event.html", message=message)
+
+@events.route("/event/<int:id>", methods=["GET", "POST","DELETE", "PATCH"])
+def event_api(id):
+    if request.method == "GET":
+        event = Event.query.get(id)
+        message=None
+    elif request.method == "POST":
+        name = request.form.get("name")
+        description = request.form.get("description")
+        date = datetime.strptime(request.form["date"], "%Y-%m-%d").date()
+        time = datetime.strptime(request.form["time"], "%H:%M").time()
+        location = request.form.get("location")
+        person = request.form.get("person")
+        new_event = Event(name=name, description=description, date=date, time=time, location=location, person= person)
+        db.session.add(new_event)
+        db.session.commit()
+        message = "Event added successfully"
+        event=new_event
+    elif request.method == "DELETE":
+        event = Event.query.get(id)
+        db.session.delete(event)
+        db.session.commit()
+        message = "Event deleted successfully"
+        return render_template("events.html", message=message)
+    elif request.method == "PATCH":
+        event = Event.query.get(id)
+        event.name = request.form.get("name")
+        event.description = request.form.get("description")
+        event.date = datetime.strptime(request.form["date"], "%Y-%m-%d").date()
+        event.time = datetime.strptime(request.form["time"], "%H:%M").time()
+        event.location = request.form.get("location")
+        event.person = request.form.get("person")
+        db.session.commit()
+        message = "Event updated successfully"
+    return render_template("detail_event.html", event=event, message=message)
--- stasks/routes/tasks.py
+++ stasks/routes/tasks.py
@@ -4,6 +4,11 @@ from datetime import datetime
 
 tasks = Blueprint('tasks', __name__)
 
+@tasks.route("/tasks")
+def get_tasks():
+    tasks = Task.query.all()
+    return render_template("tasks.html", tasks=tasks)
+
 @tasks.route("/tasks/<date>")
 def get_tasks_date(date):
     date = datetime.strptime(date, "%Y-%m-%d").date()
@@ -11,7 +16,7 @@ def get_tasks_date(date):
     return render_template("tasks.html", tasks=tasks)
 
 
-@tasks.route("/tasks", methods=["GET", "POST"])
+@tasks.route("/tasks/add", methods=["GET", "POST"])
 def add_task():
     message = ""
     if request.method == "GET":
@@ -27,33 +32,13 @@ def add_task():
         message = "Task added successfully"
     return render_template("add_task.html", message=message)
 
-@tasks.route("/task/<int:id>", methods=["DELETE", "GET", "PATCH"])
-def task_api(id):
+@tasks.route("/task/<int:id>", methods=[ "GET"])
+def task_detail(id):
     task = Task.query.get(id)
     if task is None:
         print(f"No task found with id {id}")
         return "No task found", 404
-
-    if request.method == "DELETE":
-        print(f"Deleting task {id}")
-        db.session.delete(task)
-        db.session.commit()
-        print(f"Task {id} deleted successfully")
-        return "Task deleted", 200 
-    elif request.method == "GET": 
+    else:
         # troubleshooting
         print(f"received GET request for task {id}")  
         return render_template("detail_task.html", task=task)
-    elif request.method == "PATCH":
-        # troubleshooting
-        print(f"received PATCH request for task {id}, {request.form.items()}")
-        description = request.form["taskDescription"]
-        date = datetime.strptime(request.form["taskDate"], "%Y-%m-%d").date()
-        task.description = description
-        task.date = date
-        db.session.commit()
-        message = "Task updated successfully"
-        return render_template("detail_task.html", task=task, message=message)
-    else:
-        message = "Method not allowed"
-        return render_template("detail_task.html", task=task, message=message)
