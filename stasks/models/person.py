81cbbf3512f730ce594426fc45c0477fc92b8056
Rick Dawson <dawsora@gmail.com>
2024-11-02 19:42:47 -0400

    update model and login logic

--- stasks/models/person.py
+++ stasks/models/person.py
@@ -2,7 +2,6 @@ from flask_login import UserMixin
 from datetime import date, time
 from .database import db
 
-
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(20), nullable=False)
@@ -10,7 +9,7 @@ class Person(db.Model):
     goes_by = db.Column(db.String(20))
     full_name = db.Column(db.String(40), db.Computed("first_name || ' ' || last_name"))
     phone = db.Column(db.String(20))
-    email = db.Column(db.String(40))
+    email = db.Column(db.String(40), unique=True)
     address = db.Column(db.String(40))
     city = db.Column(db.String(20))
     state = db.Column(db.String(2))
@@ -28,34 +27,35 @@ class Person(db.Model):
                 dict_[key] = str(dict_[key])
         return dict_
 
+    @staticmethod
     def get_all():
         return Person.query.order_by(Person.first_name).all()
 
+    @staticmethod
     def get_clients():
         return Person.query.filter_by(employee=False).all()
 
+    @staticmethod
     def get_client_names():
-        people = (
-            Person.query.filter_by(employee=False).with_entities(Person.full_name).all()
-        )
+        people = Person.query.filter_by(employee=False).with_entities(Person.full_name).all()
         names = [person[0] for person in people]
         return names
 
+    @staticmethod
     def get_employees():
         return Person.query.filter_by(employee=True).all()
 
+    @staticmethod
     def get_employees_names():
-        people = (
-            Person.query.filter_by(employee=True).with_entities(Person.full_name).all()
-        )
+        people = Person.query.filter_by(employee=True).with_entities(Person.full_name).all()
         names = [person[0] for person in people]
         return names
 
-
 class User(UserMixin, Person):
-    password = db.Column(db.String(20))
+    password = db.Column(db.String(128))  # Increased length for hashed passwords
     username = db.Column(db.String(20), unique=True)
     admin = db.Column(db.Boolean, default=False)
+    oidc_sub = db.Column(db.String(255), unique=True, nullable=True)  # OIDC subject identifier
 
     def is_admin(self):
         return self.admin
