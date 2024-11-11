fb1fe326900bc4361beaeed4df50598c051e8728
Rick Dawson <dawsora@gmail.com>
2024-11-10 20:11:35 -0500

    update script packages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,24 +4,11 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
   server: {
     port: 3000,
     proxy: {
-      '/auth': {
-        target: 'http://localhost:9180',
-        changeOrigin: true,
-        secure: false
-      },
-      '/api': {
-        target: 'http://localhost:9180',
-        changeOrigin: true,
-        secure: false
-      }
+      '/api': 'http://localhost:9180',
+      '/auth': 'http://localhost:9180'
     }
   }
 })
