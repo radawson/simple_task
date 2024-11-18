f58a34a1592a41685cc5198a3e1d2fbbb81885a1
Rick Dawson <dawsora@gmail.com>
2024-11-17 22:16:57 -0500

    update Toast

--- frontend/src/components/common/Toast.jsx
+++ frontend/src/components/common/Toast.jsx
@@ -1,19 +1,45 @@
-//src/components/common/Toast.jsx
-import React from 'react';
-import { MDBBtn, MDBToast } from 'mdb-react-ui-kit';
+// src/components/common/Toast.jsx
+import React, { useEffect } from 'react';
+import { MDBToast } from 'mdb-react-ui-kit';
 
 const Toast = ({ message, show, onClose }) => {
-  return (
-    <MDBToast
-      show={show}
-      autohide
-      position='top-right'
-      color='info'
-      onClose={onClose}
+  useEffect(() => {
+    if (show) {
+      const timer = setTimeout(() => {
+        onClose();
+      }, 3000); // Auto hide after 3 seconds
+      return () => clearTimeout(timer);
+    }
+  }, [show, onClose]);
+
+  return show ? (
+    <div 
+      style={{ 
+        position: 'fixed',
+        top: '20px',
+        right: '20px',
+        zIndex: 1050 
+      }}
     >
-      {message}
-    </MDBToast>
-  );
+      <MDBToast
+        show={true}
+        color='info'
+        className="mb-3"
+        autohide={false}
+        headerContent={
+          <div className="d-flex justify-content-between align-items-center">
+            <span>Notification</span>
+            <button 
+              className="btn-close" 
+              onClick={onClose}
+            />
+          </div>
+        }
+      >
+        {message}
+      </MDBToast>
+    </div>
+  ) : null;
 };
 
 export default Toast;
