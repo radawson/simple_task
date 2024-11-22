2c4aaf31d33796a7b5257e01226f6a468f69953a
Rick Dawson <dawsora@gmail.com>
2024-11-22 11:14:36 -0500

    remove old python elements

--- stasks/models/note.py
+++
@@ -1,9 +0,0 @@
-from .database import db
-
-class Note(db.Model):
-    __tablename__ = "notes"
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    content = db.Column(db.Text)
-    date = db.Column(db.Date)
-    added_by = db.Column(db.String(30), nullable=True)
